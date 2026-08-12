84291ea41603c97b1aba4a761d90c30d40d373e7
Shilin Li <56470374+Syurli@users.noreply.github.com>
2026-08-12 18:04:59 +0800

    feat(debug): route runtime pages through V11 response panel

--- Apps/Workbench/src/pages/RuntimePagesV3.tsx
+++ Apps/Workbench/src/pages/RuntimePagesV3.tsx
@@ -1,2 +1,2 @@
-export { SimulationPage, DebugPage } from './RuntimePagesV4';
+export { SimulationPage, DebugPage } from './RuntimePagesV5';
 export { VisualizationPage, ConnectionPage } from './RuntimePagesV3Base';
