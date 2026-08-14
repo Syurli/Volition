1bc9c36e7bbcbbcb58ceb76070cbf668ea98c829
Shilin Li <56470374+Syurli@users.noreply.github.com>
2026-08-14 14:09:38 +0800

    Route Workbench only through fixed tactical hierarchy

--- Apps/Workbench/src/simulation/tacticalWizardSimulationV4.ts
+++ Apps/Workbench/src/simulation/tacticalWizardSimulationV4.ts
@@ -1,4 +1,14 @@
-export * from './tacticalWizardSimulationCurrent';
-export * from './tacticalWizardExecutionOwnership';
 export * from './tacticalWizardRuntime';
+export * from './tacticalWizardHierarchy';
+export type {
+  BuddyRole,
+  CoverState,
+  GrenadeKind,
+  GrenadeVisual,
+  LocomotionMode,
+  SimulationOverlaySettings,
+  SpecialAction,
+  TacticalOpportunityPurpose,
+  TacticalTask,
+} from './tacticalWizardSimulationV7';
 export { TacticalWizardRuntime as TacticalWizardSimulation } from './tacticalWizardRuntime';
